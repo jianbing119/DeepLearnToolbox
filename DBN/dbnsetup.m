89153e8d20917936ae487ce3f767c28205c90c53
Tambet Matiisen <tambet.matiisen@gmail.com>
2015-02-03 19:55:26 +0200

    Changes per "A Practical Guide to Training Restricted Boltzmann Machines": 1. Initialize RBM weights with Gaussian with 0.01 deviation. 2. Initialize RBM biases from data: log(p*(1-p)), where p(i) is the probability of visible unit i turning on. We take it p(i) = mean(x(i)). 3. Use probabilities when performing weight update, instead of stochastic activations.

--- DBN/dbnsetup.m
+++ DBN/dbnsetup.m
@@ -6,10 +6,15 @@ function dbn = dbnsetup(dbn, x, opts)
         dbn.rbm{u}.alpha    = opts.alpha;
         dbn.rbm{u}.momentum = opts.momentum;
 
-        dbn.rbm{u}.W  = zeros(dbn.sizes(u + 1), dbn.sizes(u));
+        dbn.rbm{u}.W  = 0.01 * randn(dbn.sizes(u + 1), dbn.sizes(u));
         dbn.rbm{u}.vW = zeros(dbn.sizes(u + 1), dbn.sizes(u));
 
-        dbn.rbm{u}.b  = zeros(dbn.sizes(u), 1);
+        if u == 1
+            p = mean(x)';
+            dbn.rbm{u}.b  = log(p ./ (1 - p));
+        else
+            dbn.rbm{u}.b  = zeros(dbn.sizes(u), 1);
+        end
         dbn.rbm{u}.vb = zeros(dbn.sizes(u), 1);
 
         dbn.rbm{u}.c  = zeros(dbn.sizes(u + 1), 1);
