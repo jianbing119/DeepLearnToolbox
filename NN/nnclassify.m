16cfd03445eaed111f397570816684209b977f6a
Tambet Matiisen <tambet.matiisen@gmail.com>
2015-06-16 22:09:30 +0300

    Fixed dummy return value for Matlab.

--- NN/nnclassify.m
+++ NN/nnclassify.m
@@ -1,4 +1,4 @@
 function labels = nnclassify(nn, x)
     a = nnpredict(nn, x);
-    [_, labels] = max(a,[],2);
+    [~, labels] = max(a,[],2);
 end
--- NN/nntest.m
+++ NN/nntest.m
@@ -1,6 +1,6 @@
 function [er, bad] = nntest(nn, x, y)
     labels = nnclassify(nn, x);
-    [_, expected] = max(y,[],2);
+    [~, expected] = max(y,[],2);
     bad = find(labels ~= expected);
     er = numel(bad) / size(x, 1);
 end
